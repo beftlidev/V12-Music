13c91b3d23ef424ee5fcc42d6268fa026b178fcf
iUgur <77486662+iUgur@users.noreply.github.com>
2021-06-06 09:24:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-Yapılıyor... 
+const { MusicBot } = require('discord-music-system'); // npm i discord-music-system Yap terminal de falan
+
+client.musicBot = new MusicBot(client, {
+    ytApiKey: 'YouTube API key', // yt ap key lazım ab YouTube da videoları var
+    prefix: '!', // Botunuzun Prefixiniz yaz fln
+    language: 'en' // değiştirme Türkçe yok xd
+}); //Github: iUgur
+
+client.on('message', async message => {
+    if(message.author.bot) { //Space Giveaway Ekle yaw
+        return;
+    }; //Discord: iUgur#7464
+    client.musicBot.onMessage(message); //Yardım menüsü de var Bi dal al istersen xd
+}); //buraya eller isen çalışmaz anla yaw 
